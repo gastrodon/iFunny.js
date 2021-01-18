e41619e9314c7ae29317cdedfa5fea2be257e5db
Noah Harris <mail@gastrodon.io>
2021-01-18 00:09:02 -0500

    deno fmt

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-export { APIError } from "./objects/error.ts"
+export { APIError } from "./objects/error.ts";
 export { Client } from "./objects/client.ts";
 export { Freshable } from "./objects/freshable.ts";
