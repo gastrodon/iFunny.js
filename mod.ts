2235dc04f4ac133f8818554d6b220341829a8d23
Noah Harris <mail@gastrodon.io>
2021-01-18 00:04:40 -0500

    Create APIError

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
+export { APIError } from "./objects/error.ts"
 export { Client } from "./objects/client.ts";
 export { Freshable } from "./objects/freshable.ts";
---
+++ objects/error.ts
@@ -0,0 +1,1 @@
+export class APIError extends Error {}
