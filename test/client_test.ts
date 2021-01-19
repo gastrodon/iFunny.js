9b1dcb2145697decfb0c1e0c163b6f707dec2177
Noah Harris <mail@gastrodon.io>
2021-01-18 21:04:08 -0500

    Balance auth tests
    Balance auth between coverage and not getting ratelimited by minimizing
    the number of auths we do, if we can avoid it

--- test/client_test.ts
+++ test/client_test.ts
@@ -2,7 +2,6 @@ import {
   assertEquals,
   assertNotEquals,
   assertThrowsAsync,
-  emptyDir,
   v4,
 } from "../deps.ts";
 
@@ -10,15 +9,23 @@ import { APIError, Client } from "../mod.ts";
 
 const EMAIL: string | undefined = Deno.env.get("IFUNNYJS_EMAIL");
 const PASSWORD: string | undefined = Deno.env.get("IFUNNYJS_PASSWORD");
-const NO_AUTH: string | undefined = Deno.env.get("IFUNNYJS_NO_AUTH");
+const NO_AUTH: boolean = Deno.env.get("IFUNNYJS_NO_AUTH") === undefined;
 
 const HOME: string | undefined = Deno.env.get("HOME");
 const CONFIG_ROOT: string = HOME as string + "/.config/ifunny";
 
 const BASIC_SIZE: number = 168;
 
+let CLIENT: Client | undefined = undefined;
+
 if (HOME === undefined) {
   throw new Error("HOME must be set");
+}
+
+if (EMAIL && NO_AUTH) {
+  CLIENT = await (new Client()).login(EMAIL!);
+} else if (PASSWORD && !NO_AUTH) {
+  CLIENT = await (new Client()).login(EMAIL!, PASSWORD!);
 }
 
 Deno.test({
@@ -31,10 +38,8 @@ Deno.test({
 Deno.test({
   name: "basic",
   async fn() {
-    await emptyDir(CONFIG_ROOT);
-
     const client: Client = new Client();
-    const basic: string = await client.basic;
+    const basic: string = await client.fresh.basic;
 
     assertEquals(basic.length, BASIC_SIZE);
   },
@@ -43,10 +48,8 @@ Deno.test({
 Deno.test({
   name: "basic stored",
   async fn() {
-    await emptyDir(CONFIG_ROOT);
-
     const client: Client = new Client();
-    const basic: string = await client.basic;
+    const basic: string = await client.fresh.basic;
 
     assertEquals(basic, await client.basic);
   },
@@ -73,39 +76,39 @@ Deno.test({
   },
 });
 
+// WARNING: fresh auth
 Deno.test({
   name: "login",
-  ignore: EMAIL === undefined || PASSWORD === undefined ||
-    NO_AUTH !== undefined,
+  ignore: EMAIL === undefined || PASSWORD === undefined || NO_AUTH,
   async fn() {
     const client: Client = new Client();
-    await client.login(EMAIL!, PASSWORD!);
+    await client.login(EMAIL!, PASSWORD!, true);
     const headers: any = await client.headers;
 
     assertEquals(headers.authorization, `Bearer ${client.token}`);
   },
 });
 
+// WARNING: fresh auth
 Deno.test({
   name: "login fresh token",
-  ignore: EMAIL === undefined || PASSWORD === undefined ||
-    NO_AUTH !== undefined,
+  ignore: EMAIL === undefined || PASSWORD === undefined || NO_AUTH,
   async fn() {
-    await emptyDir(CONFIG_ROOT);
-
     const client: Client = new Client();
-    await client.login(EMAIL!, PASSWORD!);
+    await client.login(EMAIL!, PASSWORD!, true);
     const headers: any = await client.headers;
 
     assertEquals(headers.authorization, `Bearer ${client.token}`);
   },
 });
 
+// WARNING: fresh auth
 Deno.test({
   name: "login bad email",
   ignore: NO_AUTH !== undefined,
   async fn() {
     const client: Client = new Client();
+    await client.fresh.basic;
 
     await assertThrowsAsync(
       async () => {
@@ -117,15 +120,17 @@ Deno.test({
   },
 });
 
+// WARNING: fresh auth
 Deno.test({
   name: "login bad password",
   ignore: EMAIL === undefined || NO_AUTH !== undefined,
   async fn() {
     const client: Client = new Client();
+    await client.fresh.basic;
 
     await assertThrowsAsync(
       async () => {
-        await client.login(EMAIL!, v4.generate());
+        await client.login(EMAIL!, v4.generate(), true);
       },
       APIError,
       "invalid_grant: Wrong user credentials",
