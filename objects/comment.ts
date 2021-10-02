20e390dcdf995b7df1341e9ebb414cf79a0cecef
Eva <mail@gastrodon.io>
2021-10-01 22:34:03 -0400

    Revert "API: comments appear to no longer be wrapped inside a { comment: ... }"
    This reverts commit d4f042bb86880f44e07410dee50d48e2cd863269.

--- objects/comment.ts
+++ objects/comment.ts
@@ -10,4 +10,10 @@ export class Comment extends Freshable {
     this.content_id = content_id;
     this.path = `/content/${this.content_id}/comments/${this.id}`;
   }
+
+  get data(): Promise<any> {
+    return (async () => {
+      return (await super.data).comment;
+    })();
+  }
 }
